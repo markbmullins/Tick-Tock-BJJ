bafae5e5770ca5fd119b4eed6009b7a033f03e1c
Mark Mullins <markmullins95@gmail.com>
2019-08-30 13:51:18 -0400

    Update Contact.js

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Contact.scss";
 const url =
   "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3348.3891899178307!2d-80.05053068481385!3d32.940732680922785!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88fe637b874630df%3A0xbce47f921f239b88!2sTick-Tock%20Jiu-Jitsu!5e0!3m2!1sen!2sus!4v1566626932028!5m2!1sen!2sus";
+// test comment
 const Contact = () => {
   const mapSelector = () => {
     const onIOS =
