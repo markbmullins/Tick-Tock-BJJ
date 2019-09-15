c1af7fb478c13d7defb0c5d119cdf50043648263
markbmullins <markmullins95@gmail.com>
2019-09-14 22:29:02 -0400

    removing commented code

--- src/Components/Hamburger/Hamburger.js
+++ src/Components/Hamburger/Hamburger.js
@@ -3,15 +3,11 @@ import PropTypes from "prop-types";
 import "./Hamburger.scss";
 
 const Hamburger = ({ isOpen, menuClicked }) => {
-  //   const [open, setOpen] = useState(false);
   const openClass = isOpen ? "open" : "";
 
   return (
     <div className="burger-container">
-      <div
-        className={"menu-btn " + openClass}
-        // style={{ height: buttonHeight, width: buttonWidth }}
-        onClick={() => menuClicked()}>
+      <div className={"menu-btn " + openClass} onClick={() => menuClicked()}>
         <div className="menu-btn-burger"></div>
       </div>
     </div>
