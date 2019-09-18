64c3af08e4e13725ac548c9e282dda3536b2c203
markbmullins <markmullins95@gmail.com>
2019-09-18 18:30:13 -0400

    completed bottom ribbon

--- src/Components/BottomRibbon/BottomRibbon.js
+++ src/Components/BottomRibbon/BottomRibbon.js
@@ -2,15 +2,18 @@ import React from "react";
 import NavBarIcon from "../NavBarIcon/NavBarIcon";
 import "./BottomRibbon.scss";
 import { PHONE_NUMBER, PHONE_ICON, DIRECTIONS_ICON } from "../../vars";
+import { mapSelector } from "../../helpers";
 
 const ribbonItems = [
   {
     icon: <NavBarIcon imgSource={PHONE_ICON} />,
-    text: "Call Now"
+    text: "Call Now",
+    href: `tel:${PHONE_NUMBER}`
   },
   {
     icon: <NavBarIcon imgSource={DIRECTIONS_ICON} />,
-    text: "Get Directions"
+    text: "Get Directions",
+    onClick: mapSelector
   }
 ];
 
@@ -19,10 +22,18 @@ const BottomRibbon = () => {
     <div id="bottom-ribbon" className="flex-center">
       {ribbonItems.map(item => {
         return (
-          <div>
-            <div>{item.icon}</div>
-            <div>{item.text}</div>
-          </div>
+          <a href={item.href}>
+            <div
+              className="bottom-ribbon-item"
+              onClick={() => {
+                if (item.onClick) {
+                  item.onClick();
+                }
+              }}>
+              <div>{item.icon}</div>
+              <div className="bottom-ribbon-text">{item.text}</div>
+            </div>
+          </a>
         );
       })}
     </div>
