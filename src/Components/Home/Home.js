971bf1540ba65d6b78b67958dc311b599686f1de
markbmullins <markmullins95@gmail.com>
2019-09-18 18:37:36 -0400

    fixed button margins

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import Section from '../Section/Section';
-import Button from '../Button/Button';
-import './Home.scss';
-import {
-  PHONE_NUMBER,
-  BANNER_IMAGE,
-  PAGE_TITLE,
-  SUBTITLES
-} from '../../vars';
+import React from "react";
+import Section from "../Section/Section";
+import Button from "../Button/Button";
+import "./Home.scss";
+import { PHONE_NUMBER, BANNER_IMAGE, PAGE_TITLE, SUBTITLES } from "../../vars";
 
 const title = <div id="home-title">{PAGE_TITLE}</div>;
 
 const subtitle = (
   <div id="home-subtitle">
-    {SUBTITLES.map(sub => <div>{sub}</div>)}
+    {SUBTITLES.map(sub => (
+      <div>{sub}</div>
+    ))}
   </div>
 );
 
 const callButton = (
-  <div id="home-call-button">
+  <div id="home-call-button" className="flex-center">
     <Button href={`tel:${PHONE_NUMBER}`} customId="home-call-button-link">
       CALL {PHONE_NUMBER}
     </Button>
